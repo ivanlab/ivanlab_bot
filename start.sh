b5a1d85bae41ffbbd4c15a36f38161022049b754
Ivan Padilla <ivanlab@gmail.com>
2022-04-29 18:02:47 +0000

    remove sudo

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d -it --name ivanlab_bot --mount type=bind,source="$(pwd)",target=/app -p 2222:22 ivanlab_bot
+docker run -d -it --name ivanlab_bot --mount type=bind,source="$(pwd)",target=/app -p 2222:22 ivanlab_bot
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker stop ivanlab_bot
-sudo docker ps -a | grep ivanlab_bot | awk '{ print substr($1, 0, 12) }' | xargs sudo docker rm
+ docker stop ivanlab_bot
+ docker ps -a | grep ivanlab_bot | awk '{ print substr($1, 0, 12) }' | xargs docker rm
